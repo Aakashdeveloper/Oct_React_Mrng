4cee99ac88bb437294ea29a1edd5e80fe387629a
handaad <aakash.handa@natwest.com>
2020-10-14 07:03:51 +0530

    a

--- firstapp/src/component/Home.js
+++ firstapp/src/component/Home.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import JSON from './db.json';
+import NewsDisplay from './NewsDisplay';
 
-const Home = () => {
-    return(
-        <React.Fragment>
-            <Header/>
-            <h1>My React App</h1>
-            <p>This is React App for Learning</p>
-            <Footer year="2020" month="Oct"/>
-        </React.Fragment>
-    )
+class Home extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            news:JSON,
+            filtered:JSON
+        }
+    }
+    /*var a = [3,6,8,4,4,7,9]
+    a.filter((data) => {return data>5})*/
+
+    filterNews = (keyword) => {
+        const output = this.state.news.filter((data) => {
+            return data.title.toLowerCase().indexOf(keyword.toLowerCase())>-1
+        })
+        this.setState({filtered:output})
+    }
+
+    render(){
+        //console.log(this.state.news);
+        return(
+            <React.Fragment>
+                <Header userText={(userInput) => {this.filterNews(userInput)}}/>
+                <NewsDisplay newsdata={this.state.filtered}/>
+                <Footer year="2020" month="Oct"/>
+            </React.Fragment>
+        )
+    }
+    
 }
 
 export default Home;
--- javascript/first.js
+++ javascript/first.js
@@ -20,3 +20,45 @@ function add(a,b){
 
 add(2,4)
 4
+
+//map help to iterate over the data
+var a = [3,6,8,4]
+a.map((data) => {return data*2})
+[6, 12, 16, 8]
+
+//filter help to take out the value 
+//it only return those for which condition is true
+var a = [3,6,8,4,4,7,9]
+a.filter((data) => {return data>5})
+(4) [6, 8, 7, 9]
+
+var city = ['Delhi','Mumbai','Pune','Chandigarh']
+city.indexOf('Delhi')
+
+var city = ['Delhi','Mumbai','Pune','Chandigarh']
+city.indexOf('Delhi')
+0
+city.indexOf('Pune')
+2
+city.indexOf('Amsterdam')
+-1
+city.indexOf('London')
+-1
+
+//ternory operator
+
+var a = 10
+
+a>10?"Hi":"Bie"
+"Bie"
+
+a==10?"Hi":"Bie"
+"Hi"
+
+function add(a,b){
+    return a+b
+}
+
+
+add(1,2)
+
