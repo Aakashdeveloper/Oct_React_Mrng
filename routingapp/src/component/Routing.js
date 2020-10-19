478caceddbcf5d1e9adc98fde7d6971da94a57c3
Aakashdeveloper <ahanda205@gmail.com>
2020-10-19 07:08:36 +0530

    a

--- routingapp/src/component/Routing.js
+++ routingapp/src/component/Routing.js
@@ -2,20 +2,21 @@ import React from 'react';
 import Home from './Home';
 import Post from './Post';
 import Profile from './Profile';
-import {BrowserRouter,Route,Link} from 'react-router-dom';
+import {BrowserRouter,Route} from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer'
+import PostDetails from './PostDetails';
 
 const Routing = () => {
     return(
         <BrowserRouter>
             <div>
-                <header>
-                    <Link to="/">Home</Link>
-                    <Link to="/post">Post</Link>
-                    <Link to="/profile">Profile</Link>
-                </header>
+                <Header/>
                 <Route exact path="/" component={Home}/>
                 <Route path="/post" component={Post}/>
+                <Route path="/details/:topic" component={PostDetails}/>
                 <Route path="/profile" component={Profile}/>
+                <Footer/>
             </div>
         </BrowserRouter>
     )
