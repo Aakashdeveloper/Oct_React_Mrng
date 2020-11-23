a04310651f06e80443d141797fe1f070bd2c7731
Aakashdeveloper <ahanda205@gmail.com>
2020-11-23 06:44:06 +0530

    a

---
+++ redux2/src/__test__/reducer.test.js
@@ -0,0 +1,25 @@
+import reducer from '../reducer';
+
+describe('Reducer Testing',() => {
+    it('Should return initial state',() => {
+        expect(JSON.stringify(reducer(undefined,{})))
+        .toEqual(JSON.stringify({
+            article:{},
+            gallery:{}
+        }))
+    })
+
+    it('Reducer with action',() => {
+        let state={
+            article:[],
+            gallery:[]
+        }
+
+        state= reducer(state,{action:'GET_LATEST'})
+
+        expect(state).toEqual({
+            article:[],
+            gallery:[]
+        })
+    })
+})
